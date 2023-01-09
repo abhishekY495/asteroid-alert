a0bee261ec48ca52e85532b6de3324cfbe292d9f
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-09 14:33:35 +0530

    Added regex for asteroid name and Fixed calculation of asteroidSize

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const asteroidsList = document.querySelector("#asteroids");
+const regex = /\(([^)]*)\)/;
 
 const getAsteroidsData = async () => {
   const response = await fetch(".netlify/functions/getAsteroids");
@@ -10,10 +11,10 @@ const getAsteroidsData = async () => {
   console.log(asteroids);
   //
   asteroids.map((asteroid, index) => {
-    const asteroidName = asteroid.name;
+    const asteroidName = asteroid.name.match(regex)[1];
     const asteroidMaxSize = asteroid.estimated_diameter.meters.estimated_diameter_max;
     const asteroidMinSize = asteroid.estimated_diameter.meters.estimated_diameter_min;
-    const asteroidEstimatedSize = parseFloat(asteroidMaxSize - asteroidMinSize).toFixed(2);
+    const asteroidEstimatedSize = parseFloat((asteroidMaxSize + asteroidMinSize) / 2).toFixed(0);
     const asteroidApproachTime = asteroid.close_approach_data[0].close_approach_date_full.split(" ")[1];
     const isHazardous = asteroid.is_potentially_hazardous_asteroid;
     const asteroidNeoReferenceId = asteroid.neo_reference_id;
@@ -25,10 +26,10 @@ const getAsteroidsData = async () => {
         <div class="asteroid-details">
           <p>${index+1}. ${asteroidName}</p>
           <p>Estimated Size - ${asteroidEstimatedSize} m</p>
+          <p>Miss Distance - ${asteroidMissDistance.toFixed(1)} au</p>
           <p>Approach Time - ${asteroidApproachTime}</p>
-          <p>Miss Distance - ${asteroidMissDistance.toFixed(2)} au</p>
           ${
-            isHazardous 
+            isHazardous
             ? "<p class='dangerous'>Potentially Dangerous</p>"
             : "<p class='not-dangerous'>Not Dangerous</p>"
           }
