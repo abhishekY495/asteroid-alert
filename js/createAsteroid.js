a4ecbf83089d8c74dc38fd0785b9669b039fb7ac
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-17 20:02:03 +0530

    Created createAsteroid.js

---
+++ js/createAsteroid.js
@@ -0,0 +1,42 @@
+function createAsteroid(data, asteroidID) {
+  const epoch = data.orbit.epoch;
+  const orbitData = data.orbit.elements;
+  const a = orbitData.find((element) => element.name === "a");
+  const e = orbitData.find((element) => element.name === "e");
+  const i = orbitData.find((element) => element.name === "i");
+  const om = orbitData.find((element) => element.name === "om");
+  const w = orbitData.find((element) => element.name === "w");
+  const ma = orbitData.find((element) => element.name === "ma");
+  const q = orbitData.find((element) => element.name === "q");
+  const ephem = new Spacekit.Ephem(
+    {
+      a: parseFloat(a.value),
+      e: parseFloat(e.value),
+      i: parseFloat(i.value),
+      om: parseFloat(om.value),
+      w: parseFloat(w.value),
+      ma: parseFloat(ma.value),
+      q: parseFloat(q.value),
+      epoch: parseFloat(epoch),
+    },
+    "deg"
+  );
+  //
+  const asteroid = space.createShape(`asteroid${asteroidID}`, {
+    ephem,
+    shape: {
+      shapeUrl: "./assets/space/shapeData.obj",
+    },
+    rotation: {
+      enable: true,
+      lambdaDeg: 251,
+      betaDeg: -63,
+      period: 3.755067,
+      yorp: 1.9e-8,
+      phi0: 0,
+      jd0: 2443568.0,
+      speed: 0.3,
+    },
+  });
+  space.getViewer().followObject(asteroid, [-0.01, -0.01, 0.01]);
+}
