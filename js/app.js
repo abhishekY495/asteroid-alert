7a7b4fbd84c329fc7ff58f492b7c7f4307d2ed3a
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-19 19:42:05 +0530

    Adding/Retrieving asteroid data to localStorage

--- js/app.js
+++ js/app.js
@@ -1,49 +1,26 @@
-const asteroidsList = document.querySelector("#asteroids");
-const regex = /\(([^)]*)\)/;
+const date = new Date().getDate();
+const month = new Date().getMonth()+1;
+const year = new Date().getFullYear();
+const todaysDate = `${addZero(year)}-${addZero(month)}-${addZero(date)}`;
 
 const getAsteroidsData = async () => {
-  const response = await fetch(".netlify/functions/getAsteroids");
-  const data = await response.json();
-  // 
-  const nearEarthObjects = data.near_earth_objects;
-  const todaysDate = Object.keys(nearEarthObjects)[0];
-  const asteroids = nearEarthObjects[todaysDate];
-  console.log(asteroids);
-  //
-  asteroids.map((asteroid, index) => {
-    const asteroidName = asteroid.name.match(regex)[1];
-    const asteroidMaxSize = asteroid.estimated_diameter.meters.estimated_diameter_max;
-    const asteroidMinSize = asteroid.estimated_diameter.meters.estimated_diameter_min;
-    const asteroidEstimatedSize = parseFloat((asteroidMaxSize + asteroidMinSize) / 2).toFixed(0);
-    const asteroidApproachTime = asteroid.close_approach_data[0].close_approach_date_full.split(" ")[1];
-    const isHazardous = asteroid.is_potentially_hazardous_asteroid;
-    const asteroidNeoReferenceId = asteroid.neo_reference_id;
-    const asteroidSpeed = parseFloat(asteroid.close_approach_data[0].relative_velocity.kilometers_per_second);
-    const asteroidMissDistance = parseFloat(asteroid.close_approach_data[0].miss_distance.astronomical);
-    // 
-    asteroidsList.innerHTML += `
-      <li class="asteroid-container asteroid-container-${index+1}">
-        <div class="asteroid-details">
-          <p>${index+1}. ${asteroidName}</p>
-          <p>Estimated Size - ${asteroidEstimatedSize} m</p>
-          <p>Miss Distance - ${asteroidMissDistance.toFixed(1)} au</p>
-          <p>Approach Time - ${asteroidApproachTime}</p>
-          ${
-            isHazardous
-            ? "<p class='dangerous'>Potentially Dangerous</p>"
-            : "<p class='not-dangerous'>Not Dangerous</p>"
-          }
-        </div>
-        <div class="asteroid-size-comparison">${compareSize(asteroidEstimatedSize)}</div>
-        <div class="asteroid-speed-comparison">${compareSpeed(asteroidSpeed)}</div>
-        <button class="toggle-orbit-btn grid-col-span-3" onclick="asteroidOrbit(${asteroidNeoReferenceId},${index+1})">
-          Show Orbit
-        </button>
-        <div class="orbit-container orbit-container-${index+1} grid-col-span-3 hide"></div>
-      </li>
-      <br>
-    `
-  });
+  const localAsteroids = JSON.parse(localStorage.getItem("localAsteroids"));
+  const localDate = localStorage.getItem("localDate");
+  if (!localAsteroids) {
+    const response = await fetch(".netlify/functions/getAsteroids");
+    const data = await response.json();
+    localStorage.setItem("localAsteroids", JSON.stringify(data));
+    listAsteroids(data);
+  } else {
+    if (String(localDate) !== String(todaysDate)) {
+      localStorage.clear();
+      const response = await fetch(".netlify/functions/getAsteroids");
+      const data = await response.json();
+      localStorage.setItem("localAsteroids", JSON.stringify(data));
+      listAsteroids(data);
+    }
+    listAsteroids(localAsteroids);
+  }
 };
 
 getAsteroidsData();
