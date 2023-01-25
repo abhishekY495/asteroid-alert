d609d7ae498c23c09f5c2a9432d6f234c2afd38a
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-25 17:47:57 +0530

    Added date parameter

--- functions/getAsteroids.js
+++ functions/getAsteroids.js
@@ -2,19 +2,13 @@ require("dotenv").config();
 import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
-  const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const todaysDate = `${year}-${month}-${day}`;
-  //
+  const date = event.queryStringParameters.date;
   const API_URL = "https://api.nasa.gov/neo/rest/v1/feed";
   const API_KEY = process.env.NASA_API_KEY;
-  const apiURL = `${API_URL}?api_key=${API_KEY}&start_date=${todaysDate}&end_date=${todaysDate}`;
+  const apiURL = `${API_URL}?api_key=${API_KEY}&start_date=${date}&end_date=${date}`;
   //
   const response = await fetch(apiURL);
   const data = await response.json();
-  //
   return {
     statusCode: 200,
     body: JSON.stringify(data),
