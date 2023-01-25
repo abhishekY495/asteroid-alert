b25b60c816a3c6b6819867bf4373d3c4d3665cd9
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-25 17:52:52 +0530

    Fixed if..else condition and minor changes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-const date = new Date().getDate();
-const month = new Date().getMonth()+1;
-const year = new Date().getFullYear();
-const todaysDate = `${addZero(year)}-${addZero(month)}-${addZero(date)}`;
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const todaysDate = `${addZero(year)}-${addZero(month)}-${addZero(day)}`;
 
 const getAsteroidsData = async () => {
   const localAsteroids = JSON.parse(localStorage.getItem("localAsteroids"));
   const localDate = localStorage.getItem("localDate");
   if (!localAsteroids) {
-    const response = await fetch(".netlify/functions/getAsteroids");
+    const response = await fetch(`.netlify/functions/getAsteroids?date=${todaysDate}`);
     const data = await response.json();
     localStorage.setItem("localAsteroids", JSON.stringify(data));
+    localStorage.setItem("localDate", todaysDate);
     listAsteroids(data);
   } else {
     if (String(localDate) !== String(todaysDate)) {
       localStorage.clear();
-      const response = await fetch(".netlify/functions/getAsteroids");
+      const response = await fetch(`.netlify/functions/getAsteroids?date=${todaysDate}`);
       const data = await response.json();
       localStorage.setItem("localAsteroids", JSON.stringify(data));
+      localStorage.setItem("localDate", todaysDate);
       listAsteroids(data);
+    } else {
+      listAsteroids(localAsteroids);
     }
-    listAsteroids(localAsteroids);
   }
 };
 
