9a1c09f3825d6364871f66bd85677890db8032b5
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-01-30 00:35:32 +0530

    Updated date and minor changes

--- js/about.js
+++ js/about.js
@@ -1,18 +1,18 @@
 const about = document.querySelector("#about");
 const dateObj = new Date;
-const todaysDate_ = String(dateObj).split(" ").splice(0,4).join(" ");
+const todaysDate_ = String(dateObj).split(" ").splice(1,3);
 
 about.innerHTML = `
 <p>
   Asteroid alert uses 
   <a class="about-links" href="https://api.nasa.gov/" target="_blank">NASA's NeoWs API</a> 
   to provide the most recent information on asteroids that will make a close approach by 
-  Earth today i.e <span class="bold">${todaysDate_}</span>. 
+  Earth today i.e <span class="date">
+    ${todaysDate_[1]}
+    ${todaysDate_[0]}
+    ${todaysDate_[2]}</span>. 
   Click <img id="about-img" src="./assets/images/asteroid-button.png"></img> 
-  to get a 3D view of where the asteroid is in space, made using 
-  <a class="about-links" href="https://typpo.github.io/spacekit/" target="_blank">spacekit</a>. 
-  The app is open-source and can be found on 
-  <a class="about-links" href="https://github.com/abhishekY495/asteroidalert" target="_blank">github</a>. 
+  to get a 3D view of where the asteroid is in space. 
   <br>
   <div id="about-divider-container">
     <div class="about-divider"></div>
