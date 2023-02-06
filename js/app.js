e0a44d466bd4518a3b05247613723a033cc9e5ca
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2023-02-06 21:03:21 +0530

    fetch error handling

--- js/app.js
+++ js/app.js
@@ -3,33 +3,45 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const todaysDate = `${addZero(year)}-${addZero(month)}-${addZero(day)}`;
+const localAsteroids = JSON.parse(localStorage.getItem("localAsteroids"));
 
 const getAsteroidsData = async () => {
   const loadingContainer = document.querySelector(".loading-container");
-  const localAsteroids = JSON.parse(localStorage.getItem("localAsteroids"));
-  const localDate = localStorage.getItem("localDate");
   if (!localAsteroids) {
-    const response = await fetch(`.netlify/functions/getAsteroids?date=${todaysDate}`);
-    const data = await response.json();
-    localStorage.setItem("localAsteroids", JSON.stringify(data));
-    localStorage.setItem("localDate", todaysDate);
-    loadingContainer.style.display = "none";
-    listAsteroids(data);
-  } else {
-    if (String(localDate) !== String(todaysDate)) {
-      localStorage.clear();
+    try {
       const response = await fetch(`.netlify/functions/getAsteroids?date=${todaysDate}`);
       const data = await response.json();
+      console.log(data);
       localStorage.setItem("localAsteroids", JSON.stringify(data));
       localStorage.setItem("localDate", todaysDate);
       loadingContainer.style.display = "none";
       listAsteroids(data);
-    } else {
-      loadingContainer.style.display = "none";
-      listAsteroids(localAsteroids);
+      orbitBtnRotate();
+    } catch {
+      asteroidDataErrorHandler();
+    }
+  } else {
+    try {
+      const localDate = localStorage.getItem("localDate");
+      if (String(localDate) !== String(todaysDate)) {
+        localStorage.clear();
+        const response = await fetch(`.netlify/functions/getAsteroids?date=${todaysDate}`);
+        const data = await response.json();
+        console.log(data);
+        localStorage.setItem("localAsteroids", JSON.stringify(data));
+        localStorage.setItem("localDate", todaysDate);
+        loadingContainer.style.display = "none";
+        listAsteroids(data);
+        orbitBtnRotate();
+      } else {
+        loadingContainer.style.display = "none";
+        listAsteroids(localAsteroids);
+        orbitBtnRotate();
+      }
+    } catch {
+      asteroidDataErrorHandler();
     }
   }
-  orbitBtnRotate();
 };
 
 getAsteroidsData();
